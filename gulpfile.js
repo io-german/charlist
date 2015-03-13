2441b326a9a86fd1391d17c4dd454440b7e2829e
ig <ig@anahoret.com>
2015-03-13 10:57:27 +0200

    Configure gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var coffee = require('gulp-coffee');
+var gutil = require('gulp-util');
+
+gulp.task('default', ['copy', 'less', 'javascript']);
+
+gulp.task('copy', function() {
+  gulp.src('./src/html/**/*.html')
+    .pipe(gulp.dest('./target/html'));
+});
+
+gulp.task('javascript', function() {
+  gulp.src('./src/coffee/**/*.coffee')
+    .pipe(coffee().on('error', gutil.log))
+    .pipe(gulp.dest('./target/js'));
+});
 
 gulp.task('less', function() {
   gulp.src('./src/less/main.less')
---
+++ src/coffee/main.coffee
@@ -0,0 +1,5 @@
+log = (str) -> console.log(str);
+
+log(1);
+log('abc');
+log(45);
